534ac25c78f2870bfc46d9dbaf2f982b2ea1a564
KorJuk69 <korjuk1@gmail.com>
2017-09-14 17:24:19 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,18 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        System.out.print(a);
+        double den1 = scanner.nextInt();
+        int den2 = scanner.nextInt();
+        int dni=1;
+
+        while (den1<den2){
+
+            den1 = den1 * 1.1;
+            dni++;
+
+        }
+
+        System.out.print(dni);
 
     }
 }
