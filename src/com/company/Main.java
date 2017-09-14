9d39b992b5b4926bd5154350452ee8b3970c2921
KorJuk69 <korjuk1@gmail.com>
2017-09-14 17:12:08 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,22 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        System.out.print(a);
+
+        if(a!=1) {
+
+            while ((a / 2) % 2 == 0) {
+                a = a / 2;
+            }
+        }
+
+        if((a==2)||(a==1)){
+            System.out.print("Yes");
+        }
+        else {
+            System.out.print("NO");
+        }
+
+
 
     }
 }
