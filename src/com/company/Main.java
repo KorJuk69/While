1d24899f0f589c521168d7872c05ff16b9721adb
KorJuk69 <korjuk1@gmail.com>
2017-09-14 22:12:12 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,41 +8,6 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        int n=3;
-        int nM=2;
-        int nMM;
-        int i = 4;
 
-        if(a==0){
-            System.out.print(0);
-        }
-        else {
-            if(a==1){
-                System.out.print(0 + " " + 1);
-            }
-            else {
-                if(a==2){
-                    System.out.print(0 + " " + 1 + " " + 1);
-                }
-                else {
-                    if (a==3){
-                        System.out.print(0 + " " + 1 + " " + 1 + " " + 2);
-                    }
-                    else {
-
-                        System.out.print(0 + " " + 1 + " " + 1 + " " + 2 + " ");
-                        while (i<=a){
-
-                            System.out.print(n + " ");
-                            nMM = nM;
-                            nM = n;
-                            n = nM + nMM;
-                            i++;
-
-                        }
-                    }
-                }
-            }
-        }
     }
 }
