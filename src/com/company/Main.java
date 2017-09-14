b364abfd0cde5e62d82fa7586fe5f04f4dd85a00
KorJuk69 <korjuk1@gmail.com>
2017-09-14 17:31:45 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,19 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        System.out.print(a);
+        int sum1 = scanner.nextInt();
+        double proc = scanner.nextInt();
+        int sum2 = scanner.nextInt();
+        int years = 0;
+
+        while (sum1<sum2){
+
+            sum1 = (int)(sum1 * ((proc/100)+1));
+            years++;
+
+        }
+
+        System.out.print(years);
 
     }
 }
