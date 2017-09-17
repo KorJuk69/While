bc54ac58462f0f1cb5b83da8babe33f952318dac
KorJuk69 <korjuk1@gmail.com>
2017-09-17 20:57:59 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,37 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int n = scanner.nextInt();
+        int i=0;
+        int sosud1 = a;
+        int sosud2 = b;
 
+        if ((n>a)||(n>b)){
+            System.out.print("Impossible");
+        }
+        else {
+
+            while ((a!=n)||(b!=n)){
+
+                if((sosud1==sosud2)&&(sosud1!=n)){
+                    System.out.print("Impossible");
+                    break;
+                }
+                else{
+                    if (sosud1<sosud2){
+
+                        //В душе не ебу как это реализовывать
+
+                    }
+                }
+
+                i++;
+                if (i==100000){
+                    System.out.print("Impossible");
+                    break;
+                }
+            }
+        }
     }
 }
